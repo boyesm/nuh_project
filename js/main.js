6b56bf263673b760a6c5866851f5ae02c1c3f0db
adamkonzelmann <66523553+adamkonzelmann@users.noreply.github.com>
2020-06-06 22:25:03 -0400

    ZERO

--- js/main.js
+++ js/main.js
@@ -5,29 +5,29 @@
 function waterUse(){
 	var nResidents = a1;
 	var q2 = 16 * a2 * nResidents;
-	var q3 = 16 * a3 * nResidents;
+	var q3 = (16 * a3 * nResidents)/7;
 	var q4 = 8 * a4 * nResidents;
-	var q5 = 225 * a5 * nResidents;
+	var q5 = (225 * a5 * nResidents)/7;
 
 	if (q6 == "yes"){
-		var q6a = 50 * a6 * nResidents;
+		var q6a = (50 * a6 * nResidents)/7;
 	} else {
-		var q6a = 100 * a6 * nResidents;
+		var q6a = (100 * a6 * nResidents)/7;
 	}
 
 	if (q7 == "yes"){
-		var q7a = 25 * a7;
+		var q7a = (25 * a7)/7;
 	} else {
-		var q7a = 12.5 * a7;
+		var q7a = (12.5 * a7)/7;
 	}
 
 	var q8 = 160 * a8
 
 	if (q9 == "yes"){
-		var q9a = 960 * a9;
+		var q9a = (960 * a9)/30;
 	} else {
 		var q9a = 0;
 	}
 
-	return q2 + q3 + q4 + q5 + q6a + q7a + q8 + q9;
+	return (q2 + q3 + q4 + q5 + q6a + q7a + q8 + q9) * 365;
 }
